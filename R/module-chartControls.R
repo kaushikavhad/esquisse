eb6292a61c00eaec0a65474fcb26d388dc0bea1d
Kaushik Avhad <45625220+kaushikavhad@users.noreply.github.com>
2019-06-03 13:57:08 +0530

    Update module-chartControls.R

--- R/module-chartControls.R
+++ R/module-chartControls.R
@@ -98,9 +98,10 @@ chartControlsUI <- function(id) {
 chartControlsServer <- function(input, output, session, 
                                 type, data_table, data_name,
                                 ggplot_rv, 
-                                use_facet = shiny::reactive(FALSE), 
-                                use_transX = shiny::reactive(FALSE), 
-                                use_transY = shiny::reactive(FALSE)) {
+                                use_facet = shiny::reactive(FALSE) 
+                                #use_transX = shiny::reactive(FALSE), 
+                                #use_transY = shiny::reactive(FALSE)) 
+                                {
 
   ns <- session$ns
   
