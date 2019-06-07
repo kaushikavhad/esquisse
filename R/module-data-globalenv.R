64e87244d1e715a434f9e752fceb0b8200bd0ba1
Kaushik Avhad <45625220+kaushikavhad@users.noreply.github.com>
2019-06-07 15:20:34 +0530

    Update module-data-globalenv.R

--- R/module-data-globalenv.R
+++ R/module-data-globalenv.R
@@ -20,7 +20,7 @@ dataGlobalEnvUI <- function(id, dismissOnValidate = TRUE, selectVars = TRUE, coe
   # List of data.frame
   dfs <- search_obj(what = "data.frame")
   if (is.null(dfs)) {
-    dfs <- dbGetQuery(conn,"show tables")
+    dfs <- dbListTables(pool)
     #dfs <- data.frame(dbSendQuery(conn, "select * from Country"))
   }
   
