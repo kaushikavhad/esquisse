4366156b3879b19760f9ea6a0e224333ea25ca66
Kaushik Avhad <45625220+kaushikavhad@users.noreply.github.com>
2019-06-11 13:02:55 +0530

    Update esquisserUI.R

--- R/esquisserUI.R
+++ R/esquisserUI.R
@@ -1,187 +1,250 @@
-#' @title Esquisse Shiny module
-#' 
-#' @description Launch \code{esquisse} in a classic Shiny app.
-#'
-#' @param id Module's id.
-#' @param header Logical. Display or not \code{esquisse} header.
-#' @param choose_data Logical. Display or not the button to choose data.
-#' 
-#' @return A \code{reactiveValues} with 3 slots :
-#'  \itemize{
-#'   \item \strong{code_plot} : code to generate plot.
-#'   \item \strong{code_filters} : a list of length two with code to reproduce filters.
-#'   \item \strong{data} : \code{data.frame} used in plot (with filters applied).
-#'  }
-#' 
-#' @note For the module to display correctly, it is necessary to place it in a container with a fixed height.
-#'
-#' @export
-#' 
-#' @name module-esquisse
-#'
-#' @importFrom htmltools tags tagList singleton
-#' @importFrom shiny plotOutput icon actionButton NS
-#' @importFrom miniUI miniTitleBarButton miniPage
-#' @importFrom shinyWidgets prettyToggle
-#'
-#' @examples 
-#' 
-#' if (interactive()) {
-#' 
-#' 
-#' ### Part of a Shiny app ###
-#' 
-#' library(shiny)
-#' library(esquisse)
-#' 
-#' ui <- fluidPage(
-#'   tags$h1("Use esquisse as a Shiny module"),
-#'   
-#'   # Force scroll bar to appear (otherwise hidden by esquisse)
-#'   tags$style("html, body {overflow: visible !important;"),
-#'   
-#'   radioButtons(
-#'     inputId = "data", 
-#'     label = "Data to use:", 
-#'     choices = c("iris", "mtcars"),
-#'     inline = TRUE
-#'   ),
-#'   tags$div(
-#'     style = "height: 700px;", # needs to be in fixed height container
-#'     esquisserUI(
-#'       id = "esquisse", 
-#'       header = FALSE, # dont display gadget title
-#'       choose_data = FALSE # dont display button to change data
-#'     )
-#'   )
-#' )
-#' 
-#' server <- function(input, output, session) {
-#'   
-#'   data_r <- reactiveValues(data = iris, name = "iris")
-#'   
-#'   observeEvent(input$data, {
-#'     if (input$data == "iris") {
-#'       data_r$data <- iris
-#'       data_r$name <- "iris"
-#'     } else {
-#'       data_r$data <- mtcars
-#'       data_r$name <- "mtcars"
-#'     }
-#'   })
-#'   
-#'   callModule(module = esquisserServer, id = "esquisse", data = data_r)
-#'   
-#' }
-#' 
-#' shinyApp(ui, server)
-#' 
-#' 
-#' 
-#' ### Whole Shiny app ###
-#' 
-#' library(shiny)
-#' library(esquisse)
-#' 
-#' 
-#' # Load some datasets in app environment
-#' my_data <- data.frame(
-#'   var1 = rnorm(100),
-#'   var2 = sample(letters[1:5], 100, TRUE)
-#' )
-#' 
-#' 
-#' 
-#' ui <- fluidPage(
-#'   tags$div( # needs to be in fixed height container
-#'     style = "position: fixed; top: 0; bottom: 0; right: 0; left: 0;", 
-#'     esquisserUI(id = "esquisse")
-#'   )
-#' )
-#' 
-#' server <- function(input, output, session) {
-#'   
-#'   callModule(module = esquisserServer, id = "esquisse")
-#'   
-#' }
-#' 
-#' shinyApp(ui, server)
-#' 
-#' }
-#' 
-esquisserUI <- function(id, header = TRUE, choose_data = TRUE) {
-  
-  ns <- NS(id)
-  
-  
-  box_title <- tags$div(
-    class="gadget-title dreamrs-title-box",
-    tags$h1(shiny::icon("wrench"), "ggplot2 builder", class = "dreamrs-title"),
-    tags$div(
-      class = "pull-right",
-      miniTitleBarButton(inputId = ns("close"), label = "Close")
-    )
-  )
+TEMPLATE = u"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>IngenSpark</title>
+		<link rel="icon" type="image/x-icon" href="http://ingen.ingenspark.com/assets/img/4.png">
+        <!-- external libs from cdnjs -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.11/c3.min.css">
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.11/c3.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-csv/0.71/jquery.csv-0.71.min.js"></script>
+
+
+        <link rel="stylesheet" type="text/css" href="https://cdn.statically.io/gh/chandramohan1995/master/master/style1.css">
+        <script type="text/javascript" src="https://cdn.statically.io/gh/chandramohan1995/master/master/js/pivot.min.js"></script>
+        <script type="text/javascript" src="https://cdn.statically.io/gh/chandramohan1995/master/master/js/d3_renderers.min.js"></script>
+        <script type="text/javascript" src="https://cdn.statically.io/gh/chandramohan1995/master/master/js/c3_renderers.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.19.0/export_renderers.min.js"></script>
+        <script type="text/javascript" src="https://github.com/niklasvh/html2canvas/releases/download/v1.0.0-alpha.8/html2canvas.min.js"></script>
+		
+         <script type="text/javascript" src ="https://rawgit.com/eligrey/canvas-toBlob.js/master/canvas-toBlob.js"></script>
+        
+         <link rel="stylesheet" type="text/css" href="http://dev.mbrdemo.com/ingen/app.css">
+         <link rel="stylesheet" type="text/css" href="http://dev.mbrdemo.com/ingen/all.min.css">
+         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/angular-material-icons/0.7.1/angular-material-icons.min.js">
+        
+        <style>
+            body {font-family: Verdana;}
+            .node {
+              border: solid 1px white;
+              font: 10px sans-serif;
+              line-height: 12px;
+              overflow: hidden;
+              position: absolute;
+              text-indent: 2px;
+            }
+            .c3-line, .c3-focused {stroke-width: 3px !important;}
+            .c3-bar {stroke: white !important; stroke-width: 1;}
+            .c3 text { font-size: 12px; color: dodgerblue;}
+            .tick line {stroke: white;}
+            .c3-axis path {stroke: dodgerblue;}
+            .c3-circle { opacity: 1 !important; }
+            .c3-xgrid-focus {visibility: hidden !important;}
+            
+           
+            
+            
+        </style>
+    </head>
+    <body>
+	 <!--<center><img class='logo' src="http://ingen.ingenspark.com/assets/img/logo.png" alt="LOGO" height="60" width="200"></center>-->
+        
+        
+<div class="navbar_main ">
+
+
+    <!--<button class="hidemenubtn"><mat-icon>menu</mat-icon></button>-->
+
+    <div class="container">
+      <div class="row">
+          
+          <div class="header_in">
+              <div class="col-md-2">
+            <a class="logo_app">
+                <img src="http://ingen.ingenspark.com/assets/img/logo-2.png">
+            </a>
+        </div>
+
+        <div class="col-md-6">
+          <div id="custom-search-input">
+            <div class="input-group col-md-12">
+                <input type="text" class="  search-query form-control" placeholder="Search" />
+                <span class="fa fa-search"> </span>
+            </div>
+        </div>
+        </div>
+
+
+      
+          <div class="col-md-4">
+              <button class="btn admin_center_btn">Admin Center</button>
+          
+              <div class="menu_links">
+                  <div class="dropdown">
+                    <button class="btn dropdown-toggle button_pli vew_button12" type="button" id="menu1" data-toggle="dropdown">
+                       <img src="http://dev.mbrdemo.com/ingen/plus.png">
+                    </button>
+                    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Add Project</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Upload Data</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">New Report</a></li>
+                    </ul>
+                  </div>
+                  
+                  
+                  <div class="dropdown">
+                    <button class="btn dropdown-toggle button_pli vew_button" type="button" id="menu1" data-toggle="dropdown">
+                        <img src="http://ingen.ingenspark.com/assets/img/4.png">
+                    </button>
+                    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">My Projects</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">My Data</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">My Reports</a></li>
+                    </ul>
+                  </div>
+                  
+                  
+                  <div class="dropdown">
+                    <button class="btn dropdown-toggle button_pli" type="button" id="menu1" data-toggle="dropdown">
+                        <img src="http://ingen.ingenspark.com/assets/img/user.png">
+                    </button>
+                    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Help</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Logout</a></li>
+                    </ul>
+                  </div>
+                  
+                  
+              </div>
+              </div>
+          </div>
     
 
-  ### addin
-  miniPage(
+    </div>
+  </div>
+        </div>
+        
+        
+          <div class="navigation_2nd" *ngIf="showToggle">
+    
+            <ul class="menu_hide">
+              <li>
+                  <button><span><img src="http://dev.mbrdemo.com/ingen/m1.png" ></span>Ingen Data</button>
+              </li>
+              <li >
+                  <button><span><img src="http://dev.mbrdemo.com/ingen/m2.png" ></span> Ingen Connect</button>
+              </li>
+              <li>
+                  <button><span><img src="http://dev.mbrdemo.com/ingen/m3.png"></span> Ingen Customer</button>
+              </li>
+              <li>
+                  <button><span><img src="http://dev.mbrdemo.com/ingen/m4.png"></span> Ingen Optim</button>
+              </li>
+              <li>
+                  <button><span><img src="http://dev.mbrdemo.com/ingen/m5.png"></span> Ingen Tracker</button>
+              </li>
+              <li>
+                  <button><span><img src="http://dev.mbrdemo.com/ingen/m6.png"></span> Ingen Measures</button>
+              </li>
+            </ul>
 
-    # style sheet
-    singleton(x = tagList(
-      tags$link(rel="stylesheet", type="text/css", href="esquisse/styles.css"),
-      tags$script(src = "esquisse/clipboard/clipboard.min.js")
-    )),
+          </div>
 
-    if (isTRUE(header)) box_title,
-    # page
-    layoutAddin(
-      top_left = htmltools::tagList(
-        htmltools::tags$div(
-          style = "padding: 10px;",
-          dropInput(
-            inputId = ns("geom"),
-            choicesNames = geomIcons()$names, 
-            choicesValues = geomIcons()$values,
-            dropWidth = "290px",
-            width = "100%"
-          ),
-          if (isTRUE(choose_data)) chooseDataUI(id = ns("choose-data"))
-        )
-      ),
-      top_right = dragulaInput(
-        inputId = ns("dragvars"), 
-        sourceLabel = "Variables", 
-        targetsLabels = c("X", "Y", "Fill", "Color", "Size", "Group", "Facet"), 
-        targetsIds = c("xvar", "yvar", "fill", "color", "size", "group", "facet"),
-        choices = "",
-        badge = FALSE, 
-        width = "100%", 
-        height = "100%",
-        replace = TRUE
-      ),
-      main = htmltools::tags$div(
-        style = "margin-top: 10px; padding-bottom: 25px; height: 100%;",
-        tags$div(
-          style = "position: absolute; right: 0; top: 10px; font-weight: bold; z-index: 1000;",
-          prettyToggle(
-            inputId = ns("play_plot"), 
-            value = TRUE,
-            label_on = "Play",
-            label_off = "Pause",
-            outline = TRUE,
-            plain = TRUE,
-            bigger = TRUE, 
-            inline = TRUE,
-            icon_on = icon("play-circle-o", class = "fa-2x"),
-            icon_off = icon("pause-circle-o", class = "fa-2x")
-          )
-        ),
-        shiny::plotOutput(outputId = ns("plooooooot"), width = "100%", height = "100%")
-      )
-    ),
+        
+        
+	 <form class="actionbtns" action="fulltable" method="post">
+         <div class="main_container main_container2">
+             
+             <ul>
+                <li><button class="btn cmbtn" Onclick ="simple()">  image  </button></li>
+                <li><button class= "button1 cmbtn btn" name="fulltable" type="submit" formtarget="_blank">show full table</button></li>
+                <li><button class="pull-right btn" id="download">download</button></li>
+                    
+             </ul>
+                
+         </div>
+    
+        
+        </form>
 
-    chartControlsUI(id = ns("controls"))
-  )
+	
+     <script>
+	 function simple(){
+	 html2canvas(document.querySelector(".pvtRendererArea")).then(function(canvas) {
 
-}
+     //canvas.style.width = "1000px";
+     //canvas.style.height = "500px";
+    document.body.appendChild(canvas);
+});
+	 }
+	 </script>	
+	 
+  
+  
+		<script>
+		
+            $(function(){
+                if(window.location != window.parent.location)
+                    $("<a>", {target:"_blank", href:""})
+                        .text("[pop out]").prependTo($("body"));
+
+                $("#output").pivotUI(
+                    $.csv.toArrays($("#output").text()),
+                    $.extend({
+                        renderers: $.extend(
+							
+                            $.pivotUtilities.renderers,
+							$.pivotUtilities.plotly_renderers,
+                            $.pivotUtilities.c3_renderers,
+                            $.pivotUtilities.d3_renderers,
+                            $.pivotUtilities.export_renderers							
+                            ),
+                        hiddenAttributes: [""]
+                    }, %(kwargs)s)
+                ).show();
+             });
+        </script>
+		
+        <div class="main_container" id="output" style="display: none;">%(csv)s</div>
+		
+
+       
+	<div class="full_width footer" >
+
+  <div class="container">
+      <div class="row">
+          <div class="col-md-6">
+
+            <p>Copyright 2018 · All rights reserved</p>
+          </div>
+          <div class="col-md-6">
+
+            <img src="http://ingen.ingenspark.com/assets/img/logo.png">
+          </div>
+      </div>
+  </div>
+
+</div>
+        
+  
+    </body>
+	<script>
+var canvas = document.getElementsByTagName('canvas');
+var context = canvas[0].getContext('2d');
+download.addEventListener("click", function() {
+  // only jpeg is supported by jsPDF
+  var imgData = canvas.toDataURL("image/jpeg", 1.0);
+  var pdf = new jsPDF();
+
+  pdf.addImage(imgData, 'JPEG', 0, 0);
+  pdf.save("download.pdf");
+}, false);
+	  </script>
+</html>
+"""
